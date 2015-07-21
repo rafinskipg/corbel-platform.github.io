4d0c735b53a4c4cecd223f9bbc1d3dedf805c173
Alberto J. Rubio <yebenes9@gmail.com>
2015-07-21 11:12:08 +0200

    Update configuration to use corbel.io domain.

--- travis-runner.sh
+++ travis-runner.sh
@@ -13,5 +13,5 @@ then
   git init && \
   git add . && \
   git commit -m "deploy" && \
-  git push --force --quiet "https://${GH_OAUTH_TOKEN}@github.com/bq/corbel-doc" master:gh-pages > /dev/null 2>&1
+  git push --force --quiet "https://${GH_OAUTH_TOKEN}@github.com/corbel-platform/corbel-doc" master:gh-pages > /dev/null 2>&1
 fi
